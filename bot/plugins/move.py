7aa928c7f2b0c18be603b997340c487b834d6aca
kissyouhunter <kissyouhunter@hotmail.com>
2022-07-04 22:39:53 +0800

    Update move.py

--- bot/plugins/move.py
+++ bot/plugins/move.py
@@ -1,3 +1,4 @@
+from distutils.file_util import move_file
 import logging
 import subprocess
 
@@ -7,3 +8,5 @@ from telegram.ext import CommandHandler, CallbackContext
 #subprocess.call(["bash", "./upload.sh"], shell=False)
 
 print('this is a test python script!')
+
+updater.add_handler(CommandHandler('move', move_file_to_clould), bot_command=BotCommand("move", "移动文件或文件夹到网盘"))
