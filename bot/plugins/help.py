07a38d3e5f76b70200aa5fad03ba8b117bd1d10b
kissyouhunter <kissyouhunter@hotmail.com>
2022-07-04 23:22:29 +0800

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -26,16 +26,16 @@ HELP_MESSAGE = """<b>Commands</b>:
 • /version: 获取qbittorrent和API的版本
 
 <i>WRITE commands</i>
-• <code>.torrent</code> document: add torrent by file
-• magnet url: add a torrent by magnet url
+• <code>.torrent</code> document: 上传种子文件直接添加任务
+• magnet url: 添加磁力链接直接添加任务
 
 <i>EDIT commands</i>
 • /altdown: change the alternative max download speed from a keyboard
 • /altdown <code>[kb/s]</code>: change the alternative max download speed
 • /altup <code>[kb/s]</code>: change the alternative max upload speed
-• /pauseall: pause all torrents
-• /resumeall: resume all torrents
-• /set <code>[setting] [new value]</code>: change a setting
+• /pauseall: 暂停所有种子
+• /resumeall: 恢复所有种子
+• /set <code>[setting] [new value]</code>: 改变 qbittorrent 设置
 • <code>+tag</code> or <code>-tag</code>: reply to a torrent info message with "<code>+some tags</code>" or \
 "<code>-some tags</code>" to add/remove tags. Multiple tags can be passed, separated by a comma \
 (tags can have white spaces)
