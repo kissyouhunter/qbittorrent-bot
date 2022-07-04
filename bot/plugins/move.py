d636524c589c72c967424d94d39f449f11226aba
kissyouhunter <kissyouhunter@hotmail.com>
2022-07-04 23:52:10 +0800

    uodate

--- bot/plugins/move.py
+++ bot/plugins/move.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 #subprocess.call(["bash", "./upload.sh"], shell=False)
 
 def move_file_to_cloud(update: Update, context: CallbackContext):
-    subprocess.call(["bash", "./move.sh"], shell=False)
+    subprocess.call(["bash", "/app/bot/plugins/move.sh"], shell=False)
     #update.subprocess.call(['bash', './move.sh', shell=False])
 
 updater.add_handler(CommandHandler('move', move_file_to_cloud), bot_command=BotCommand("move", "移动文件或文件夹到网盘"))
