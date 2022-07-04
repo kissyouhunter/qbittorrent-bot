ca4b8c25aab52431d4c7d73811114908c47fe69f
kissyouhunter <kissyouhunter@hotmail.com>
2022-07-04 23:35:02 +0800

    update

--- bot/plugins/move.py
+++ bot/plugins/move.py
@@ -9,9 +9,7 @@ from bot.updater import updater
 
 logger = logging.getLogger(__name__)
 
-#subprocess.call(["bash", "./upload.sh"], shell=False)
-
 def move_file_to_cloud(update: Update, context: CallbackContext):
-    update.message.reply_text('this is a test python script!')
+    subprocess.call(["bash", "./upload.sh"], shell=False)
 
 updater.add_handler(CommandHandler('move', move_file_to_cloud), bot_command=BotCommand("move", "移动文件或文件夹到网盘"))
