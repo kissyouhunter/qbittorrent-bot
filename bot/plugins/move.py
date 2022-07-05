61bf741617111619b52d51711d653ab9e92b1b3f
kissyouhunter <kissyouhunter@hotmail.com>
2022-07-05 22:23:26 +0800

    update

--- bot/plugins/move.py
+++ bot/plugins/move.py
@@ -1,19 +1,23 @@
 import logging
 import os
 import subprocess
-
+from config import config
 from telegram import Update, BotCommand
 from telegram.ext import CommandHandler, CallbackContext
 
 from bot.updater import updater
 
 logger = logging.getLogger(__name__)
+od_name = config.rclone.name
+up_path = config.rclone.up_path
+source = '/Downloads'
 
 def move_file_to_cloud(update: Update, context: CallbackContext):
-    #logger.info('executing move shell script', update.message.from_user.first_name)
+    cmd = 'rclone moveto -v -P \"{}\" \"{}\":\"{}\"'.format(source, od_name, up_path)
 
-    subprocess.call(["bash", "bot/plugins/move.sh"], shell=False)
+    os.system(cmd)
 
-    update.message.reply_text('文件上传完毕')
+    update.message.reply_text('上传完毕')
+
 
 updater.add_handler(CommandHandler('move', move_file_to_cloud), bot_command=BotCommand("move", "移动文件或文件夹到网盘"))
